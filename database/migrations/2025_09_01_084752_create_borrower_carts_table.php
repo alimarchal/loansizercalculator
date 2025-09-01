d94c27060ab6a9728ab960f29af97b9288b4fa88
Ali Raza Marchal <alirazamarchal@hotmail.com>
2025-09-01 15:27:53 +0500

    feat: Update borrower_carts migration to use UUID for primary key and add additional fields

--- database/migrations/2025_09_01_084752_create_borrower_carts_table.php
+++ database/migrations/2025_09_01_084752_create_borrower_carts_table.php
@@ -4,15 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('borrower_carts', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->integer('credit_score')->nullable();
+            $table->unsignedTinyInteger('fix_and_flip_rental_experience')->comment('Years of experience')->nullable();
+            $table->unsignedTinyInteger('new_construction_experience')->comment('Years of experience')->nullable();
+            $table->string('borrower_name')->nullable();
+            $table->string('borrower_email')->nullable();
+            $table->string('borrower_phone')->nullable();
+            $table->string('broker_name')->nullable();
+            $table->string('broker_email')->nullable();
+            $table->string('broker_phone')->nullable();
+            $table->decimal('broker_points', 5, 2)->nullable();
+            $table->userTracking();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
