7541e5e93ffa288c7d520e1393a48b72faf89900
Ali Raza Marchal <alirazamarchal@hotmail.com>
2025-09-09 23:35:03 +0500

    fix: Correct FICO band maximum values in FicoBandSeeder for accuracy

--- database/seeders/FicoBandSeeder.php
+++ database/seeders/FicoBandSeeder.php
@@ -18,7 +18,8 @@ class FicoBandSeeder extends Seeder
             ['fico_range' => '680-699', 'fico_min' => 680, 'fico_max' => 699],
             ['fico_range' => '700-719', 'fico_min' => 700, 'fico_max' => 719],
             ['fico_range' => '720-739', 'fico_min' => 720, 'fico_max' => 739],
-            ['fico_range' => '740+', 'fico_min' => 740, 'fico_max' => 1000],
+            ['fico_range' => '740+', 'fico_min' => 740, 'fico_max' => 759],
+            ['fico_range' => '760+', 'fico_min' => 760, 'fico_max' => 850],
         ];
 
         FicoBand::upsert($bands, ['fico_range', 'fico_min', 'fico_max']);
